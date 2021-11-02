39b99b8910183e0efb84507d2430d05823d29614
Rafał Klinowski <klinowskirafal@gmail.com>
2021-11-03 00:55:15 +0100

    Add project files.

---
+++ AsmInCsharp.CS/Algorithms.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AsmInCsharp.CS
+{
+	public class Algorithms
+	{
+		[DllImport(@"C:\Programowanie\AsmInCsharp\x64\Debug\AsmInCsharp.Asm.dll")]
+		public static extern int Example(ulong[] data);
+	}
+}
---
+++ AsmInCsharp.CS/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace AsmInCsharp.CS
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void RunAsmButton_Click(object sender, RoutedEventArgs e)
+		{
+			var data = new ulong[4] { 0, 0, 0, 0 };
+
+			Algorithms.Example(data);
+
+			string newDataString = data[0].ToString();
+
+
+			ResultBlock.Text = $"newDataString = {newDataString}";
+		}
+	}
+}
